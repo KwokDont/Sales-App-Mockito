ecd49fa221f3b89b392f9d66ce5be217d7f692d4
GUOED3 <EDDY.GUO@OOCL.COM>
2019-08-19 15:26:20 +0800

    init & refactor: init test case and extract some methods

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -7,45 +7,27 @@ import java.util.List;
 
 public class SalesApp {
 
+    SalesDao salesDao = new SalesDao();
+
+    SalesReportDao salesReportDao = new SalesReportDao();
+
+    EcmService ecmService = new EcmService();
+
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
-		SalesDao salesDao = new SalesDao();
-		SalesReportDao salesReportDao = new SalesReportDao();
+
 		List<String> headers = null;
 		
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
-		
-		if (salesId == null) {
-			return;
-		}
+		if (salesId == null) { return; }
 		
 		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
-		Date today = new Date();
-		if (today.after(sales.getEffectiveTo())
-				|| today.before(sales.getEffectiveFrom())){
-			return;
-		}
+
+        if (this.isEffective(sales)){ return; }
 		
 		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-		
-		for (SalesReportData data : reportDataList) {
-			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {
-					if (isSupervisor) {
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
-				}
-			}
-		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
+
+        List<SalesReportData> filteredReportDataList = generateFilteredReportDataList(isSupervisor, reportDataList);
+
+        filteredReportDataList = generateTemplateList(maxRow, reportDataList);
 		
 		if (isNatTrade) {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
@@ -54,13 +36,41 @@ public class SalesApp {
 		}
 		
 		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
+
 		ecmService.uploadDocument(report.toXml());
 		
 	}
 
-	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+    protected List<SalesReportData> generateTemplateList(int maxRow, List<SalesReportData> reportDataList) {
+        List<SalesReportData> tempList = new ArrayList<SalesReportData>();
+        for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
+            tempList.add(reportDataList.get(i));
+        }
+        return tempList;
+    }
+
+    protected List<SalesReportData> generateFilteredReportDataList(boolean isSupervisor, List<SalesReportData> reportDataList) {
+        List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();;
+        for (SalesReportData data : reportDataList) {
+            if ("SalesActivity".equalsIgnoreCase(data.getType())) {
+                if (data.isConfidential()) {
+                    if (isSupervisor) {
+                        filteredReportDataList.add(data);
+                    }
+                }else {
+                    filteredReportDataList.add(data);
+                }
+            }
+        }
+        return filteredReportDataList;
+    }
+
+    protected boolean isEffective(Sales sales) {
+        Date today = new Date();
+        return today.after(sales.getEffectiveTo()) || today.before(sales.getEffectiveFrom());
+    }
+
+    protected SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -2,15 +2,52 @@ package sales;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
 public class SalesAppTest {
 
 	@Test
 	public void testGenerateReport() {
 
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
+		String salesId = "Eddy";
+		Sales sales = spy(new Sales());
+		SalesApp salesApp = spy(new SalesApp());
+		sales.setSupervisor(false);
+
+		SalesDao salesDao = mock(SalesDao.class);
+		given(salesDao.getSalesBySalesId(salesId)).willReturn(sales);
+
+		doReturn(true).when(salesApp).isEffective(any());
+
+		List<SalesReportData> reportDataList = new ArrayList<>();
+		SalesReportData salesReportData = new SalesReportData();
+		reportDataList.add(salesReportData);
+
+		SalesReportDao salesReportDao = mock(SalesReportDao.class);
+		given(salesReportDao.getReportData(sales)).willReturn(reportDataList);
+
+		List<String> headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
+
+		SalesActivityReport report = mock(SalesActivityReport.class);
+		given(report.toXml()).willReturn("XML String");
+
+		doReturn(report).when(salesApp).generateReport(headers, reportDataList);
+
+		salesApp.generateSalesActivityReport("Eddy", 10, false, false);
+
+		EcmService ecmService = spy(new EcmService());
+
+		verify(salesDao, times(1)).getSalesBySalesId("Eddy");
+		verify(salesApp, times(1)).isEffective(sales);
+		verify(salesReportDao, times(1)).getReportData(sales);
+		verify(salesApp, times(1)).generateReport(headers, reportDataList);
+		verify(report, times(1)).toXml();
+		verify(ecmService, times(1)).uploadDocument("XML String");
 		
 	}
-
-
 }
